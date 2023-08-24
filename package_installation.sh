216743965c566d23878bd1f82fe0ef87fbfd9d5d
Aniebiet Micheal Ezekiel <90110919+eamicheal@users.noreply.github.com>
2023-08-24 15:19:08 +0200

    Update package_installation.sh
    added cloudpickle

--- package_installation.sh
+++ package_installation.sh
@@ -5,9 +5,7 @@ sudo apt-get update
 sudo apt-get update -y 
 
 # Install required packages via apt-get including LLVM
-# sudo apt-get install -y python3 python3-dev python3-setuptools gcc build-essential cmake libtinfo-dev zlib1g-dev libedit-dev libxml2-dev llvm-4.0
-sudo apt-get install -y python3 python3-dev python3-setuptools gcc build-essential cmake libtinfo-dev zlib1g-dev libedit-dev libxml2-dev python3.7 llvm-4.0
+sudo apt-get install -y python3 python3-dev python3-setuptools gcc build-essential cmake libtinfo-dev zlib1g-dev libedit-dev libxml2-dev python3.7 llvm-4.0 python3-cloudpickle git
 
 # Get TVM via Git
-# git clone --recursive https://github.com/eamicheal/TVMCloned080222 tvm
-git clone --recursive https://github.com/eamicheal/tvmcloned020822 tvm
+# git clone --recursive https://github.com/eamicheal/tvmcloned020822 tvm
